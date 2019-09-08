e2314f442abbccd3d1f0c1b2967953b0db7748ea
David Danko <dcdanko@gmail.com>
2019-09-08 13:41:03 -0400

    Separate dev cli, distance add function

--- ariesk/cli_dev.py
+++ ariesk/cli_dev.py
@@ -40,17 +40,19 @@ def calculate_kmer_dists_cluster(kmer_len, num_kmers, outfile, kmer_table):
 
 @dev_cli.command('add-rotation-dists')
 @click.option('-d', '--dimensions', default=8)
+@click.option('-k', '--kmer-cols', nargs=2, default(1, 2))
 @click.option('-o', '--outfile', default='-', type=click.File('w'))
 @click.argument('rotation', type=click.Path())
 @click.argument('dist_table', type=click.File('r'))
-def add_rotation_dists(dimensions, outfile, rotation, dist_table):
+def add_rotation_dists(dimensions, kmer_cols, outfile, rotation, dist_table):
     """Add rotation distances to an existing distance table."""
     header = dist_table.readline().strip() + f',rotation_dist_{dimensions}\n'
     outfile.write(header)
     ramifier = RotatingRamifier.from_file(dimensions, rotation)
     for line in dist_table:
         line = line.strip()
-        k1, k2 = line.split(',')[2:4]
+        tkns = line.split(',')
+        k1, k2 = tkns[kmer_cols[0]], tkns[kmer_cols[1]]
         rft1, rft2 = ramifier.ramify(k1), ramifier.ramify(k2)
         d = np.linalg.norm(rft1 - rft2)
         outfile.write(line + f',{d}\n')
